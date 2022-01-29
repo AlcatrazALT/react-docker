523d69777e1bdcf1738451a2cc91556ece6fb50c
Oleg Mazalevsky <omazalevsky@technologika.onmicrosoft.com>
2022-01-29 12:17:18 +0700

    feat: use env file

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>Docker</p>
+        <p>{`Environment 1 ${process.env.REACT_APP_NAME}`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
